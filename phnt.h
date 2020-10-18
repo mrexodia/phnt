0e43d487c78636fd438cba9f204d73713673b35f
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-10-18 15:10:38 +0200

    Convert to header-only

--- phnt.h
+++ phnt.h
@@ -70,46 +70,46 @@ extern "C" {
 #endif
 
 #if (PHNT_MODE != PHNT_MODE_KERNEL)
-#include <phnt_ntdef.h>
-#include <ntnls.h>
-#include <ntkeapi.h>
+#include "phnt_ntdef.h"
+#include "ntnls.h"
+#include "ntkeapi.h"
 #endif
 
-#include <ntldr.h>
-#include <ntexapi.h>
+#include "ntldr.h"
+#include "ntexapi.h"
 
-#include <ntmmapi.h>
-#include <ntobapi.h>
-#include <ntpsapi.h>
+#include "ntmmapi.h"
+#include "ntobapi.h"
+#include "ntpsapi.h"
 
 #if (PHNT_MODE != PHNT_MODE_KERNEL)
 #include <cfg.h>
-#include <ntdbg.h>
-#include <ntioapi.h>
-#include <ntlpcapi.h>
-#include <ntpfapi.h>
-#include <ntpnpapi.h>
-#include <ntpoapi.h>
-#include <ntregapi.h>
-#include <ntrtl.h>
+#include "ntdbg.h"
+#include "ntioapi.h"
+#include "ntlpcapi.h"
+#include "ntpfapi.h"
+#include "ntpnpapi.h"
+#include "ntpoapi.h"
+#include "ntregapi.h"
+#include "ntrtl.h"
 #endif
 
 #if (PHNT_MODE != PHNT_MODE_KERNEL)
 
-#include <ntseapi.h>
-#include <nttmapi.h>
-#include <nttp.h>
-#include <ntxcapi.h>
+#include "ntseapi.h"
+#include "nttmapi.h"
+#include "nttp.h"
+#include "ntxcapi.h"
 
-#include <ntd3dkmt.h>
-#include <ntwow64.h>
+#include "ntd3dkmt.h"
+#include "ntwow64.h"
 
-#include <ntlsa.h>
-#include <ntsam.h>
+#include "ntlsa.h"
+#include "ntsam.h"
 
-#include <ntmisc.h>
+#include "ntmisc.h"
 
-#include <ntzwapi.h>
+#include "ntzwapi.h"
 
 #endif
 
